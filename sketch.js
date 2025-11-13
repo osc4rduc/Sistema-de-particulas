d02acd9a07e86385058ff4ba63f3f07556992bb0
Oscar Duchitanga <oscar.duchitangad@ucuenca.edu.ec>
2025-11-13 14:53:54 -0500

    Copia de código de pelotas

---
+++ sketch.js
@@ -0,0 +1,18 @@
+const NP = 500;
+let pelotas = [];
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    rectMode(CENTER);  
+    for(let i = 0; i < NP; i++){
+        let nuevaPelota = new Pelota();
+        pelotas[i] = nuevaPelota;     
+}
+}
+function draw(){
+    background(150);  
+    for(let i = 0; i < NP; i++){
+        pelotas[i].actualizar();
+        pelotas[i].visualizar();
+    }
+}
+
